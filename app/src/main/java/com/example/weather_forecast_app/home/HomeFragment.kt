8009104dddb98ea2db7ae72b23ba827642844f7f
thcainthry <hasani.florinda@gmail.com>
2023-05-16 14:42:38 +0200

    pamjen e ndreqa gjysen

---
+++ app/src/main/java/com/example/weather_forecast_app/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.weather_forecast_app.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weather_forecast_app.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+    lateinit var binding : FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
